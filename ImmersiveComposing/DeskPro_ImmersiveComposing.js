78cfd75cd93d7f9cc3b348cee5cec052b5aab970
ChrisW0lf <chriswol@cisco.com>
2021-05-28 09:15:29 +0200

    HEADER added

--- ImmersiveComposing/DeskPro_ImmersiveComposing.js
+++ ImmersiveComposing/DeskPro_ImmersiveComposing.js
@@ -1,4 +1,28 @@
-const xapi = require('xapi');
+/**
+ * @author Magnus Ohnm <mohm@cisco.com>, Christian Wolf <chriswol@cisco.com>
+ * @copyright Copyright (c) 2021 Cisco and/or its affiliates.
+ * @license Cisco Sample Code License, Version 1.1
+ */
+
+/**
+ * @license
+ * Copyright (c) 2021 Cisco and/or its affiliates.
+ *
+ * This software is licensed to you under the terms of the Cisco Sample
+ * Code License, Version 1.1 (the "License"). You may obtain a copy of the
+ * License at
+ *
+ *                https://developer.cisco.com/docs/licenses
+ *
+ * All use of the material herein must be in accordance with the terms of
+ * the License. All rights not expressly granted by the License are
+ * reserved. Unless required by applicable law or agreed to separately in
+ * writing, software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ */
+
+import xapi from 'xapi';
 
 function unsetGUIValue(guiId) {
     xapi.command('UserInterface Extensions Widget UnsetValue', {
@@ -68,5 +92,3 @@ xapi.event.on('UserInterface Extensions Widget Action', e => {
       }
     }
 });
- 
-//Author Magnus Ohm
