4d874b895175649b7f1f4e4563414e92c1e635e1
ChrisW0lf <chriswol@cisco.com>
2021-05-31 10:11:45 +0200

    Code and UI enhancement

--- ImmersiveComposing/DeskPro_ImmersiveComposing.js
+++ ImmersiveComposing/DeskPro_ImmersiveComposing.js
@@ -34,9 +34,9 @@ function composePresentation(sources) {
     xapi.command('Presentation Start', {
         ConnectorId: sources,
         SendingMode: 'LocalRemote' // Options are LocalOnly or LocalRemote (which starts sharing imediately)
-    }).catch(e => {
+    }).catch(exception => {
         xapi.command('UserInterface Message TextLine Display', {
-            Text: e.message,
+            Text: exception.message,
             duration: 3
         });
     });
@@ -46,49 +46,54 @@ function composeVideo(sources, parts) {
     xapi.command('Video Input SetMainVideoSource', {
         ConnectorId: sources,
         Layout: layout
-    }).catch(e => {
+    }).catch(exception => {
       xapi.command('UserInterface Message TextLine Display', {
-        Text: e.message,
+        Text: exception.message,
         duration: 3
       });
     });
 }
-xapi.event.on('UserInterface Extensions Widget Action', e => {
-    if (e.Type == 'released' && e.WidgetId.includes('_compositor')) {
-      switch (e.WidgetId.replace('_compositor', '')) {
+xapi.event.on('UserInterface Extensions Widget Action', event => {
+    if (event.WidgetId.includes('_compositor') && event.Type === 'released') {
+      switch (event.WidgetId.replace('_compositor', '')) {
         case 'stopPresentation':
           xapi.command('Presentation Stop')
           break;
+        case 'presentation-s_1':
+          const sourcecomboS1 = event.Value;
+          composePresentation(sourcecomboS1);
+          unsetGUIValue(event.WidgetId);
+          break;
         case 'presentation-s_2_e_1':
-          const sourcecomboS2E1 = e.Value.split('-');
+          const sourcecomboS2E1 = event.Value.split('-');
           composePresentation(sourcecomboS2E1);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
           break;
         case 'presentation-s_2_e_2':
-          const sourcecomboS2E2 = e.Value.split('-');
+          const sourcecomboS2E2 = event.Value.split('-');
           composePresentation(sourcecomboS2E2);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
           break;
         case 'presentation-s_2_e_3':
-          const sourcecomboS2E3 = e.Value.split('-');
+          const sourcecomboS2E3 = event.Value.split('-');
           composePresentation(sourcecomboS2E3);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
           break;
         case 'presentation-s_3_e_1':
-          const sourcecomboS3E1 = e.Value.split('-');
+          const sourcecomboS3E1 = event.Value.split('-');
           composePresentation(sourcecomboS3E1);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
           break;
         case 'presentation-s_3_e_2':
-          const sourcecomboS3E2 = e.Value.split('-');
+          const sourcecomboS3E2 = event.Value.split('-');
           composePresentation(sourcecomboS3E2);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
           break;
         default:
-          const parts = e.WidgetId.split('_');
-          const videoSourceCombo = e.Value.split('-');
+          const parts = event.WidgetId.split('_');
+          const videoSourceCombo = event.Value.split('-');
           composeVideo(videoSourceCombo,parts);
-          unsetGUIValue(e.WidgetId);
+          unsetGUIValue(event.WidgetId);
       }
     }
 });
--- ImmersiveVideo/ImmersiveVideo.js
+++ ImmersiveVideo/ImmersiveVideo.js
@@ -106,95 +106,96 @@ xapi.status.on('Cameras Background', (event) => {
 });
 
 xapi.event.on('UserInterface Extensions Widget Action', (event) => { 
-
-  // Event to change source for immersive Video
-  if (event.WidgetId === 'source_immersiveVideo') {
-    source = event.Value
-    if (isImmersiveVideo) {immersiveVideo()};
-  }
-  // Event on Sliders to update UI and ImmersiveVideo
-  else if (event.WidgetId.includes('Slider_immersiveVideo') && event.Type === 'changed') {
-    let widget = event.WidgetId.replace('Slider_immersiveVideo', '');
-    let lable;
-
-    switch (event.WidgetId) {
-      case 'sizeSlider_immersiveVideo':
-        lable = Math.round(event.Value * maxSizeOpacity / 255);
-        if (lable == 0) {lable = 1};
-        immersiveVideoParams.Scale = lable;
-        if (isImmersiveVideo) {immersiveVideo()};
-        break;
-      case 'opacitySlider_immersiveVideo':
-        lable = Math.round(event.Value * maxSizeOpacity / 255);
-        immersiveVideoParams.Opacity = lable;
-        if (isImmersiveVideo) {immersiveVideo()};
-        break;
-      case 'posAdjustmentSlider_immersiveVideo':
-        lable = Math.round(event.Value * maxMovement / 255 / 100) * 100;
-        positionMovement = lable;
-      default:
-        // do nothing
-    };
-    updateSliderUI(widget, lable);
-  }
-  // Preset position of immersive video
-  else if (event.WidgetId === 'position_immersiveVideo' && event.Type === 'released') {
-    switch (event.Value) {
-      case 'upperLeft':
-        immersiveVideo(1000, 2500);
-        break;
-      case 'upperRight':
-        immersiveVideo(8000, 2500);
-        break;
-      case 'upperCenter':
-        immersiveVideo(5000, 2500);
-        break;
-      case 'lowerCenter':
-        immersiveVideo(5000, 7500);
-        break;
-      case 'lowerLeft':
-        immersiveVideo(1000, 7500);
-        break;
-      case 'lowerRight':
-        immersiveVideo(8000, 7500);
-        break;
-      case 'activate':
-        immersiveVideo();
-        break;
-      case 'reset':
-        if (isImmersiveVideo) {immersiveVideo(8000, 7500, 50, 100);}
-        else {immersiveVideoParams = {X: 8000, Y: 7500, Scale: 50, Opacity: 100}};
-        updateSliderUI('size', immersiveVideoParams.Scale);
-        updateSliderUI('opacity', immersiveVideoParams.Opacity);
-        updateSliderUI('posAdjustment', 1000);
-        break;
-      case 'deactivate':
-        disableImmersiveVideo();
-        break;
-      default:
-        // do nothing
+  if (event.WidgetId.includes('_immersiveVideo')) {
+    // Event to change source for immersive Video
+    if (event.WidgetId === 'source_immersiveVideo') {
+      source = event.Value
+      if (isImmersiveVideo) {immersiveVideo()};
     }
-    setTimeout(() => { unsetGUIValue(event.WidgetId); }, 200);
-  }
-  // Update position of immersive video
-  else if (event.WidgetId == 'posAdjustmentPad_immersiveVideo' && event.Type == 'released') {
-    switch (event.Value) {
-      case 'left':
-        if(immersiveVideoParams.X - positionMovement >= 0) {immersiveVideoParams.X -= positionMovement};
-        break;
-      case 'right':
-        if(immersiveVideoParams.X + positionMovement <= 10000) {immersiveVideoParams.X += positionMovement};
-        break;
-      case 'up':
-        if(immersiveVideoParams.Y - positionMovement >= 0) {immersiveVideoParams.Y -= positionMovement};
-        break;
-      case 'down':
-        if(immersiveVideoParams.Y + positionMovement <= 10000) {immersiveVideoParams.Y += positionMovement};
-        break;
-      default:
-        //do nothing
+    // Event on Sliders to update UI and ImmersiveVideo
+    else if (event.WidgetId.includes('Slider_immersiveVideo') && event.Type === 'changed') {
+      let widget = event.WidgetId.replace('Slider_immersiveVideo', '');
+      let lable;
+
+      switch (event.WidgetId) {
+        case 'sizeSlider_immersiveVideo':
+          lable = Math.round(event.Value * maxSizeOpacity / 255);
+          if (lable == 0) {lable = 1};
+          immersiveVideoParams.Scale = lable;
+          if (isImmersiveVideo) {immersiveVideo()};
+          break;
+        case 'opacitySlider_immersiveVideo':
+          lable = Math.round(event.Value * maxSizeOpacity / 255);
+          immersiveVideoParams.Opacity = lable;
+          if (isImmersiveVideo) {immersiveVideo()};
+          break;
+        case 'posAdjustmentSlider_immersiveVideo':
+          lable = Math.round(event.Value * maxMovement / 255 / 100) * 100;
+          positionMovement = lable;
+        default:
+          // do nothing
+      };
+      updateSliderUI(widget, lable);
+    }
+    // Preset position of immersive video
+    else if (event.WidgetId === 'position_immersiveVideo' && event.Type === 'released') {
+      switch (event.Value) {
+        case 'upperLeft':
+          immersiveVideo(1000, 2500);
+          break;
+        case 'upperRight':
+          immersiveVideo(8000, 2500);
+          break;
+        case 'upperCenter':
+          immersiveVideo(5000, 2500);
+          break;
+        case 'lowerCenter':
+          immersiveVideo(5000, 7500);
+          break;
+        case 'lowerLeft':
+          immersiveVideo(1000, 7500);
+          break;
+        case 'lowerRight':
+          immersiveVideo(8000, 7500);
+          break;
+        case 'activate':
+          immersiveVideo();
+          break;
+        case 'reset':
+          if (isImmersiveVideo) {immersiveVideo(8000, 7500, 50, 100);}
+          else {immersiveVideoParams = {X: 8000, Y: 7500, Scale: 50, Opacity: 100}};
+          updateSliderUI('size', immersiveVideoParams.Scale);
+          updateSliderUI('opacity', immersiveVideoParams.Opacity);
+          updateSliderUI('posAdjustment', 1000);
+          break;
+        case 'deactivate':
+          disableImmersiveVideo();
+          break;
+        default:
+          // do nothing
+      }
+      setTimeout(() => { unsetGUIValue(event.WidgetId); }, 200);
+    }
+    // Update position of immersive video
+    else if (event.WidgetId == 'posAdjustmentPad_immersiveVideo' && event.Type == 'released') {
+      switch (event.Value) {
+        case 'left':
+          if(immersiveVideoParams.X - positionMovement >= 0) {immersiveVideoParams.X -= positionMovement};
+          break;
+        case 'right':
+          if(immersiveVideoParams.X + positionMovement <= 10000) {immersiveVideoParams.X += positionMovement};
+          break;
+        case 'up':
+          if(immersiveVideoParams.Y - positionMovement >= 0) {immersiveVideoParams.Y -= positionMovement};
+          break;
+        case 'down':
+          if(immersiveVideoParams.Y + positionMovement <= 10000) {immersiveVideoParams.Y += positionMovement};
+          break;
+        default:
+          //do nothing
+      };
+      if(isImmersiveVideo) {immersiveVideo()};
     };
-    if(isImmersiveVideo) {immersiveVideo()};
   };
 });
 
